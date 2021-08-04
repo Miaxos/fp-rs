ed6d951cdf054a31164ca85393faf7005c87d1d7
Miaxos <an.griffon@gmail.com>
2021-08-04 13:33:29 +0000

    fix: crates name

---
+++ target/package/fp-ts-0.0.1/src/lib.rs
@@ -0,0 +1,19 @@
+//! # Functional Programming for Rust
+//!  <div align="center">
+//!  <!-- CI -->
+//!  <img src="https://github.com/Miaxos/fp-rs/actions/workflows/ci.yml/badge.svg" />
+//!  <!-- Crates version -->
+//!  <a href="https://crates.io/crates/fp-rs">
+//!    <img src="https://img.shields.io/crates/v/fp-rs.svg?style=flat-square"
+//!    alt="Crates.io version" />
+//!  </a>
+//!  <!-- Downloads -->
+//!  <a href="https://crates.io/crates/fp-rs">
+//!    <img src="https://img.shields.io/crates/d/fp-rs.svg?style=flat-square"
+//!      alt="Download" />
+//!  </a>
+//! </div>
+//!
+//!
+
+pub struct WIP(());
---
+++ target/package/fp-ts-0.0.1/target/debug/libfp_ts.d
@@ -0,0 +1,1 @@
+/home/wizard/Dev/fp-rs/target/package/fp-ts-0.0.1/target/debug/libfp_ts.rlib: /home/wizard/Dev/fp-rs/target/package/fp-ts-0.0.1/src/lib.rs
